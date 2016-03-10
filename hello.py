a9e92d908795cf93be761f4d3de394f9afc161d7
Ruslan-Popytchenko <ruark@ukr.net>
2016-03-10 11:57:07 +0200

    ds

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-from cgi import parse_qm
+
 def app(environ, start_responce):
     start_responce('200 OK', [('Content-Type', 'text/plain')])
-    qs = parse_qs(environ['QUERY_STRING'])
-    return ['%s=%s<br>' % (k, qs[k][0]) for k in qs]
+    resp = environ['QUERY_STRING'].split("&")
+    resp = [item + "\r\n" for item in resp]
+    return resp
